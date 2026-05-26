2444ade0ea0df143f471e0240e0027934b0ea3cf
Kenneth Bogner <tuxagon91@gmail.com>
2026-05-26 19:26:35 -0400

    Migrate pack tooling to packs/_source/ + unified script

---
+++ scripts/packs.js
@@ -0,0 +1,16 @@
+import { compilePack, extractPack } from "@foundryvtt/foundryvtt-cli";
+import { readdir } from "node:fs/promises";
+
+const SRC = "packs/_source";
+const DEST = "packs";
+const [, , action, only] = process.argv;
+const names = only ? [only] : await readdir(SRC);
+
+for (const name of names) {
+  if (action === "pack") {
+    await compilePack(`${SRC}/${name}`, `${DEST}/${name}`, { yaml: true });
+  }
+  if (action === "unpack") {
+    await extractPack(`${DEST}/${name}`, `${SRC}/${name}`, { yaml: true });
+  }
+}
